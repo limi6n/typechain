ef9f85da805325bad7c4b10fd894a13dbdd3583c
limi6n <6nlimitless@gmail.com>
2021-05-02 18:30:50 +0900

    ts-watch setting

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string):void => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi("Limi", 33, "female");
+
+export {};
